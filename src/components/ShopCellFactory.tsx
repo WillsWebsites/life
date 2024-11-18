5f8395650f2910095a1b16bd8c0e3a44e13d9fe9
WillsWebsites <contactwillswebsites@gmail.com>
2024-11-17 20:58:08 -0700

    More work on factories

--- src/components/ShopCellFactory.tsx
+++ src/components/ShopCellFactory.tsx
@@ -1,6 +1,8 @@
 import { useMotherNature } from '@/hooks/useMotherNature'
 import { ICellFactory } from '@/types/cellFactory'
+import { Wrench } from 'lucide-react'
 import { Button } from './ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
 
 type Props =
   | {
@@ -12,16 +14,17 @@ type Props =
     }
 
 const ShopCellFactory = ({ variant = 'default', ...props }: Props) => {
+  const isDefault = variant === 'default' && 'factory' in props
   const globalCellCount = useMotherNature((state) => state.cellCount)
   const purchaseCellFactory = useMotherNature((state) => state.purchaseCellFactory)
 
   return (
     <div
       key={variant === 'default' && 'factory' in props ? props.factory.id : 'initial'}
-      className="flex flex-col items-center justify-center px-6 py-4 rounded-md bg-gray-800/80 min-w-[100px] min-h-[100px] border-gray-200/60 border-[1px]"
+      className="flex flex-col items-start justify-start px-2 py-4 rounded-md bg-gray-800/80 min-w-[100px] min-h-[100px] border-gray-200/60 border-[1px] relative"
     >
       {variant === 'initial' && (
-        <div className="text-left flex flex-col gap-2">
+        <>
           <span>
             Cells: <span className="font-orbitron font-bold">{globalCellCount}</span>
           </span>
@@ -37,14 +40,42 @@ const ShopCellFactory = ({ variant = 'default', ...props }: Props) => {
               Create
             </Button>
           )}
-        </div>
+        </>
       )}
 
-      {variant === 'default' && 'factory' in props && (
+      {isDefault && (
         <>
-          <span>Level: {props.factory.level}</span>
-          <span>Capacity: {props.factory.maxCells}</span>
-          <span>Cells: {props.factory.cells}</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                className="absolute top-[1px] right-[1px] p-2 h-6 hover:bg-slate-400/20 hover:text-white rounded-sm"
+              >
+                <Wrench />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-32">
+              <DropdownMenuItem
+                onClick={() => {
+                  console.log('upgrade')
+                }}
+                className="cursor-pointer flex justify-between items-center"
+                disabled={globalCellCount < 500}
+              >
+                <span>Upgrade</span>
+                <span className="text-xs text-gray-400">500</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <span>
+            Level: <span className="font-orbitron font-bold">{props.factory.level}</span>
+          </span>
+          <span>
+            Capacity: <span className="font-orbitron font-bold">{props.factory.maxCells}</span>
+          </span>
+          <span>
+            Cells: <span className="font-orbitron font-bold">{props.factory.cells}</span>
+          </span>
         </>
       )}
     </div>
